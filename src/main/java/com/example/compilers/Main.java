52031ca5446266f9f494a254759796b2f7f48a9d
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 21:43:45 -0300

    Classe Parser

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -1,5 +1,6 @@
 package com.example.compilers;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         String input = "8+5-7+9";
---
+++ src/main/java/com/example/compilers/Parser.java
@@ -0,0 +1,15 @@
+package com.example.compilers;
+
+public class Parser {
+    private byte[] input;
+    private int current; 
+
+	public Parser (byte[] input) {
+        this.input = input;
+    }
+
+    public void parse () {
+        // aqui ainda ira o código
+    }
+
+}
