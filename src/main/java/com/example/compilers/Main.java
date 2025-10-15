37c4791f86ee96af918ee1aa408466d8f2942cd5
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 21:57:53 -0300

    Teste da execução do Scanner

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -3,10 +3,13 @@ package com.example.compilers;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = "8+5-7+9";
-        Parser p = new Parser (input.getBytes());
-        p.parse();
-        
+        String input = "4-8+6";
+        Scanner scan = new Scanner (input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
 
     }
 }
