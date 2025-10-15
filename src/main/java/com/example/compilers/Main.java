48fc97fa6e8b37e2726d8fa573fdb155308fe8d3
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 22:32:40 -0300

    Teste de Execução Main

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -1,11 +1,14 @@
 package com.example.compilers;
 
+import com.example.compilers.enumss.TokenType;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String input = "45  + preco - 876";
-        Parser p = new Parser (input.getBytes());
-        p.parse();
+        Scanner scan = new Scanner (input.getBytes());
+        for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
+        System.out.println(tk);
+    }
 
     }
 }
