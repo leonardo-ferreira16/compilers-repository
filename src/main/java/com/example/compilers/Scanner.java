e8e6dba0207370b22867e95658155a568681f241
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 21:54:03 -0300

    Criada a Classe Scanner

---
+++ src/main/java/com/example/compilers/Scanner.java
@@ -0,0 +1,12 @@
+package com.example.compilers;
+
+public class Scanner {
+
+    private byte[] input;
+    private int current; 
+
+	public Scanner (byte[] input) {
+        this.input = input;
+    }
+
+}
