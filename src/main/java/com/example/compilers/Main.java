630a78b168f26685827685179c4814bca84c86be
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 22:05:36 -0300

    Atualização da Classe Main

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -3,13 +3,9 @@ package com.example.compilers;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = "4-8+6";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
+        String input = "8+5-7+9";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
 
     }
 }
