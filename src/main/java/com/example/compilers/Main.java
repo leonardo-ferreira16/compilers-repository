9608469aac51f36bd6d8d322dbe177cffe17b9b3
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 22:13:12 -0300

    Teste do Scan na Classe Main

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -3,9 +3,15 @@ package com.example.compilers;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = "8+5-7+9";
-        Parser p = new Parser (input.getBytes());
-        p.parse();
+        String input = "289-85+0+69";
+        Scanner scan = new Scanner (input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
 
     }
 }
