dce453ea30a702b20e3513dd1ead684da9c691a7
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-14 21:38:09 -0300

    Classe Main - implementação base

--- src/main/java/com/example/compilers/Main.java
+++ src/main/java/com/example/compilers/Main.java
@@ -1,7 +1,10 @@
 package com.example.compilers;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String input = "8+5-7+9";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
+
     }
 }
